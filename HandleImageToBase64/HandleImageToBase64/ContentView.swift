d04c4c0024bad8c0f8c14d0a40b413d90854d52e
James <james@kountopay.com>
2022-03-10 12:04:20 +0700

    created project

---
+++ HandleImageToBase64/HandleImageToBase64/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HandleImageToBase64
+//
+//  Created by Dr.Truong on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HandleImageToBase64/HandleImageToBase64/HandleImageToBase64App.swift
@@ -0,0 +1,17 @@
+//
+//  HandleImageToBase64App.swift
+//  HandleImageToBase64
+//
+//  Created by Dr.Truong on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HandleImageToBase64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
